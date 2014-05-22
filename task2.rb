12c71c7bbc147ac8edb1b0198fc320ca2d13b57e
Kudryashov <hekpomamohtehok@gmail.com>
2014-05-22 14:18:31 +0300

    task2 finish

--- task2.rb
+++ task2.rb
@@ -11,13 +11,11 @@ end
 # (For our purposes, a vowel is any of the A, E, I, O, U)
 # NOTE: be sure it works for both upper and lower case and for nonletters!
 def starts_with_vowel?(str)
-  for i in ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-    if i == str[0]
-      true
-    elsif str[0] == nil
-      "nonletters"
-    else false
-    end
+  if ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"].include?(str[0])
+    true
+  elsif str[0] == nil
+    "nonletters"
+  else false
   end
 end
 
